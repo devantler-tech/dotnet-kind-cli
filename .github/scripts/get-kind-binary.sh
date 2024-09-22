9f4eb5d70ced9304736bb1f4e3b7c8c0616512f7
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-09-22 19:38:00 +0200

    fix

--- .github/scripts/get-kind-binary.sh
+++ .github/scripts/get-kind-binary.sh
@@ -8,7 +8,6 @@ get() {
   local target_name=$4
   local isTar=$5
 
-  # check if tar
   if [ "$isTar" = true ]; then
     curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
     mv "$target_dir/$binary" "${target_dir}/$target_name"
@@ -18,27 +17,8 @@ get() {
   chmod +x "$target_dir/$target_name"
 }
 
-#!/bin/bash
-set -e
-
-get() {
-  local url=$1
-  local binary=$2
-  local target_dir=$3
-  local target_name=$4
-  local isTar=$5
-
-  if [ "$isTar" = true ]; then
-    curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
-    mv "$target_dir/$binary" "${target_dir}/$target_name"
-  elif [ "$isTar" = false ]; then
-    curl -LJ "$url" -o "$target_dir/$target_name"
-  fi
-  chmod +x "$target_dir/$target_name"
-}
-
-get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=arm64" "Kind" "Devantler.KindCLI/runtimes/osx-arm64/native" "kind-osx-arm64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=amd64" "Kind" "Devantler.KindCLI/runtimes/osx-x64/native" "kind-osx-x64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=amd64" "Kind" "Devantler.KindCLI/runtimes/linux-x64/native" "kind-linux-x64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=arm64" "Kind" "Devantler.KindCLI/runtimes/linux-arm64/native" "kind-linux-arm64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=arm64" "kind-darwin-arm64" "Devantler.KindCLI/runtimes/osx-arm64/native" "kind-osx-arm64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=amd64" "kind-darwin-amd64" "Devantler.KindCLI/runtimes/osx-x64/native" "kind-osx-x64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=arm64" "kind-linux-arm64" "Devantler.KindCLI/runtimes/linux-arm64/native" "kind-linux-arm64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=amd64" "kind-linux-amd64" "Devantler.KindCLI/runtimes/linux-x64/native" "kind-linux-x64" false
 get "https://getbin.io/kubernetes-sigs/kind?os=windows&arch=amd64" "kind-windows-amd64" "Devantler.KindCLI/runtimes/win-x64/native" "kind-win-x64.exe" false
