e670ed74bbeb5304625684708bad820498d26bc0
Nikolai Emil Damm <neq@energinet.dk>
2025-03-02 01:09:09 +0100

    chore: update paths to KindCLI binaries in get-kind-binary.sh script
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- .github/scripts/get-kind-binary.sh
+++ .github/scripts/get-kind-binary.sh
@@ -23,8 +23,8 @@ get() {
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=arm64" "kind-darwin-arm64" "Devantler.KindCLI/runtimes/osx-arm64/native" "kind-osx-arm64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=amd64" "kind-darwin-amd64" "Devantler.KindCLI/runtimes/osx-x64/native" "kind-osx-x64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=arm64" "kind-linux-arm64" "Devantler.KindCLI/runtimes/linux-arm64/native" "kind-linux-arm64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=amd64" "kind-linux-amd64" "Devantler.KindCLI/runtimes/linux-x64/native" "kind-linux-x64" false
-get "https://getbin.io/kubernetes-sigs/kind?os=windows&arch=amd64" "kind-windows-amd64" "Devantler.KindCLI/runtimes/win-x64/native" "kind-win-x64.exe" false
+get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=arm64" "kind-darwin-arm64" "src/Devantler.KindCLI/runtimes/osx-arm64/native" "kind-osx-arm64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=darwin&arch=amd64" "kind-darwin-amd64" "src/Devantler.KindCLI/runtimes/osx-x64/native" "kind-osx-x64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=arm64" "kind-linux-arm64" "src/Devantler.KindCLI/runtimes/linux-arm64/native" "kind-linux-arm64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=linux&arch=amd64" "kind-linux-amd64" "src/Devantler.KindCLI/runtimes/linux-x64/native" "kind-linux-x64" false
+get "https://getbin.io/kubernetes-sigs/kind?os=windows&arch=amd64" "kind-windows-amd64" "src/Devantler.KindCLI/runtimes/win-x64/native" "kind-win-x64.exe" false
